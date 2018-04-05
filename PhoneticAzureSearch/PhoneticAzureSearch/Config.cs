808bab180f725737010f811037c6d20d20028c65
Andres Rubio <arhandres@hotmail.com>
2018-04-04 19:37:39 -0500

    Load

---
+++ PhoneticAzureSearch/PhoneticAzureSearch/Config.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace PhoneticAzureSearch
+{
+    public static class Config
+    {
+        private static Lazy<IConfiguration> _configuration = new Lazy<IConfiguration>(() =>
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+            var configuration = builder.Build();
+            return configuration;
+        });
+
+        public static T GetValue<T>(string key, T defaultValue)
+        {
+            var value = _configuration.Value[key];
+
+            if (string.IsNullOrEmpty(value))
+                return defaultValue;
+
+            try
+            {
+                if (IsSimple(typeof(T)))
+                {
+                    return (T)Convert.ChangeType(value, typeof(T));
+                }
+                else
+                {
+                    return JsonConvert.DeserializeObject<T>(value);
+                }
+            }
+            catch (Exception)
+            {
+                return defaultValue;
+            }
+        }
+
+        public static T GetValue<T>(string key)
+        {
+            return GetValue<T>(key, default(T));
+        }
+
+        private static bool IsSimple(Type type)
+        {
+            if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>))
+                return IsSimple(type.GetGenericArguments()[0]);
+
+            return type.IsPrimitive
+              || type.IsEnum
+              || type.Equals(typeof(string))
+              || type.Equals(typeof(decimal));
+        }
+    }
+}
---
+++ PhoneticAzureSearch/PhoneticAzureSearch/Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+using PhoneticAzureSearch.Model;
+using System;
+using System.Collections.Generic;
+
+namespace PhoneticAzureSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static CustomAnalyzer CreatePhoneticCustomAnalyzer()
+        {
+            var analyzer = new CustomAnalyzer
+            {
+                TokenFilters = new List<TokenFilterName> { TokenFilterName.Phonetic, TokenFilterName.AsciiFolding, TokenFilterName.Lowercase },
+                Tokenizer = TokenizerName.Standard,
+                Name = "PhoneticCustomnAlyzer"
+            };
+
+            return analyzer;
+        }
+
+        static Index CreateIndexDefinition()
+        {
+            var phoneticAnalizer = CreatePhoneticCustomAnalyzer();
+
+            var definition = new Index()
+            {
+                Name = Config.GetValue<string>("AzureSearch:AzureSearchIndexName"),
+                Fields = FieldBuilder.BuildForType<IndexProduct>(),
+                Analyzers = new List<Analyzer> { phoneticAnalizer },
+            };
+
+            return definition;
+        }
+
+        static ISearchIndexClient CreateIndexAndGetClient()
+        {
+            var searchServiceName = Config.GetValue<string>("AzureSearch:AzureSearchServiceName");
+            var apiKey = Config.GetValue<string>("AzureSearch:AzureSearchKey");
+
+            var serviceClient = new SearchServiceClient(searchServiceName, new SearchCredentials(apiKey));
+            var index = CreateIndexDefinition();
+
+            if (!serviceClient.Indexes.Exists(index.Name))
+                serviceClient.Indexes.Create(index);
+
+            var indexClient = serviceClient.Indexes.GetClient(index.Name);
+
+            return indexClient;
+        }
+    }
+}
